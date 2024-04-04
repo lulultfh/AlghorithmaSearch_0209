f424640ac0228f958ab4468650ad4d3bde84621f
lulultfh <punyalulu05@gmail.com>
2024-04-04 15:36:08 +0700

    add step5

--- 209_search/209_search.cpp
+++ 209_search/209_search.cpp
@@ -53,6 +53,9 @@ void LinearSearch()
             }
             i++; //step 4
         }
+
+        if (i == n) //step 5
+            cout << "\n" << item << " not found in the array\n";
     }
 }
 
