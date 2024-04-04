fe84b5750252a411c018f51ede9b0106397b89ac
lulultfh <punyalulu05@gmail.com>
2024-04-04 14:48:41 +0700

    add looping while(true) di dalam prosedur input

--- 209_search/209_search.cpp
+++ 209_search/209_search.cpp
@@ -8,6 +8,10 @@ int arr[20]; //array to be searched
 int n; //number of elements in the array
 int i; //index of array element
 
+void input() {
+    while (true)
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
